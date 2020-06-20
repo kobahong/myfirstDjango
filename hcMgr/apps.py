13fe29a4d4c1be0ec9c6e1ffc6bf2b109463b7a3
takayuki <kobahong@gmail.com>
2020-06-20 23:33:09 +0900

    HCapps, first comit

---
+++ hcMgr/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HcmgrConfig(AppConfig):
+    name = 'hcMgr'
---
+++ hcMgr/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    params = {
+        'title':'Hello/Index',
+        'msg':'これは、サンプルで作ったページです。',
+    }
+    return render(request, 'hcMgr/index.html', params)
+# Create your views here.
